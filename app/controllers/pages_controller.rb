09312f4f6038c1bc492342a02aea7f4ed58e85d5
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2016-12-23 14:48:20 -0500

    add the first Kinetic fetch, via xapp token

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def home
+    Kinetic::GravityModel.xapp_token = Rails.application.config_for(:gravity)['xapp_token']
+    @artworks = Artwork.list visible: true, sort: '-published_at'
   end
 end
