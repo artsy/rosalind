3652314d76fa581e11f73387918e6c7403b219aa
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2016-12-23 14:46:13 -0500

    log verbosely from Foreman
    but also fix janky asset logging when using STDOUT

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,7 +43,8 @@ Rails.application.configure do
   config.assets.debug = true
 
   # Suppress logger output for asset requests.
-  config.assets.quiet = true
+  # Disabled until this gets merged: https://github.com/rails/sprockets-rails/pull/381
+  # config.assets.quiet = true
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
@@ -51,4 +52,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
 end
--- config/puma.rb
+++ config/puma.rb
@@ -45,3 +45,5 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+log_requests true
