45ed1c978ca579e524dbcebf1a4b27750b4c8812
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2017-01-06 12:18:12 -0500

    add a placeholder artwork page
    this is where we will build our first React component
    to test the new js pipeline

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   def home
     @artworks = Artwork.list visible: true, sort: '-published_at'
   end
+
+  def artwork
+    @artwork = Artwork.find params[:id]
+  end
 end
