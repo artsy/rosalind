061283241fb8945eea8ca7c71ea383020858eae9
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2017-01-11 18:00:11 -0500

    add a (failing) snapshot test to the spec

--- webpack/components/Artwork/__tests__/Artwork.spec.js
+++ webpack/components/Artwork/__tests__/Artwork.spec.js
@@ -1,3 +1,12 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import Artwork from 'components/Artwork'
+
 test('true is true', () => {
   expect(true).toBe(true)
+})
+
+it('renders correctly', () => {
+  const tree = renderer.create(<Artwork />).toJSON()
+  expect(tree).toMatchSnapshot()
 })
