6d0e50ce170b2a764d425b2a2cff310dbb4eaf6a
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2017-01-15 11:02:17 -0500

    WIP woo first fetch working

--- webpack/apps/janky-prototype/components/App.js
+++ webpack/apps/janky-prototype/components/App.js
@@ -3,10 +3,10 @@ import React from 'react'
 import SearchForm from './SearchForm.js'
 import SearchResults from './SearchResults.js'
 import './App.css'
-// import { ELASTICSEARCH_HOST, ELASTICSEARCH_AUTH_HEADER } from './secrets.js'
+import { ELASTICSEARCH_HOST, ELASTICSEARCH_AUTH_HEADER } from './secrets.js'
 
 const findByName = (items, item) => items.find(i => i.name === item.name)
-// const getArtworksFromResponse = (esResponse) => esResponse.hits.hits.map(hit => hit._source)
+const getArtworksFromResponse = (esResponse) => esResponse.hits.hits.map(hit => hit._source)
 
 class App extends React.Component {
   constructor (props) {
@@ -32,62 +32,62 @@ class App extends React.Component {
     // this.onPreviewNext = this.onPreviewNext.bind(this)
   }
 
-  // fetchArtworks() {
-  //   if ((this.state.genes.length === 0) && (this.state.tags.length === 0)) {
-  //     this.setState({ artworks: [] })
-  //   } else {
-  //     const query = this.buildElasticSearchQuery()
-  //     const uri = `${ELASTICSEARCH_HOST}/gravity_application_production/artwork/_search`
-  //     const headers = new Headers({ "Authorization": ELASTICSEARCH_AUTH_HEADER })
-  //     const method = 'post'
-  //     const body = JSON.stringify(query)
-  //     console.log('fetching', query)
-  //     fetch(uri, { headers, method, body }).then(response => {
-  //       if (response.ok) {
-  //         response.json().then(data => {
-  //           this.setState({ artworks: getArtworksFromResponse(data) })
-  //         })
-  //       } else {
-  //         console.error('Fetch error', response.statusText)
-  //       }
-  //     })
-  //   }
-  // }
+  fetchArtworks () {
+    if ((this.state.genes.length === 0) && (this.state.tags.length === 0)) {
+      this.setState({ artworks: [] })
+    } else {
+      const query = this.buildElasticSearchQuery()
+      const uri = `${ELASTICSEARCH_HOST}/gravity_application_production/artwork/_search`
+      const headers = new window.Headers({ 'Authorization': ELASTICSEARCH_AUTH_HEADER })
+      const method = 'post'
+      const body = JSON.stringify(query)
+      console.log('fetching', query)
+      window.fetch(uri, { headers, method, body }).then(response => {
+        if (response.ok) {
+          response.json().then(data => {
+            this.setState({ artworks: getArtworksFromResponse(data) })
+          })
+        } else {
+          console.error('Fetch error', response.statusText)
+        }
+      })
+    }
+  }
 
-  // buildElasticSearchQuery() {
-  //   const geneMatches = this.state.genes.map(g => { return { "match" : { "genes" : g.name } } })
-  //   const tagMatches = this.state.tags.map(t => { return { "match" : { "tags" : t.name } } })
-  //   const { filters } = this.state
-  //   let statusFilters = []
-  //   if (filters.published !== null) statusFilters = [...statusFilters, { term: { published: filters.published }} ]
-  //   if (filters.deleted !== null) statusFilters = [...statusFilters, { term: { deleted: filters.deleted }} ]
-  //   if (filters.genomed !== null) statusFilters = [...statusFilters, { term: { genomed: filters.genomed }} ]
-  //   return {
-  //     "query": {
-  //       "bool": {
-  //         "must": geneMatches.concat(tagMatches),
-  //         "filter": statusFilters
-  //       },
-  //     },
-  //     "sort": { "created_at" : "desc" },
-  //     "size": 100
-  //   }
-  // }
+  buildElasticSearchQuery () {
+    const geneMatches = this.state.genes.map(g => { return { 'match': { 'genes': g.name } } })
+    const tagMatches = this.state.tags.map(t => { return { 'match': { 'tags': t.name } } })
+    // const { filters } = this.state
+    let statusFilters = []
+    // if (filters.published !== null) statusFilters = [ ...statusFilters, { term: { published: filters.published }} ]
+    // if (filters.deleted !== null) statusFilters = [ ...statusFilters, { term: { deleted: filters.deleted }} ]
+    // if (filters.genomed !== null) statusFilters = [ ...statusFilters, { term: { genomed: filters.genomed }} ]
+    return {
+      'query': {
+        'bool': {
+          'must': geneMatches.concat(tagMatches),
+          'filter': statusFilters
+        }
+      },
+      'sort': { 'created_at': 'desc' },
+      'size': 100
+    }
+  }
 
-  // componentWillMount() {
-  //   if (this.state.genes.length || this.state.tags.length) {
-  //     this.fetchArtworks()
-  //   }
-  // }
+  componentWillMount () {
+    if (this.state.genes.length || this.state.tags.length) {
+      this.fetchArtworks()
+    }
+  }
 
-  // componentDidUpdate(prevProps, prevState) {
-  //   if ((this.state.genes !== prevState.genes) || (this.state.tags !== prevState.tags) || (this.state.filters !== prevState.filters)) {
-  //     console.log('gotta fetch')
-  //     this.fetchArtworks()
-  //   } else {
-  //     console.log('no fetch')
-  //   }
-  // }
+  componentDidUpdate (prevProps, prevState) {
+    if ((this.state.genes !== prevState.genes) || (this.state.tags !== prevState.tags) || (this.state.filters !== prevState.filters)) {
+      console.log('gotta fetch')
+      this.fetchArtworks()
+    } else {
+      console.log('no fetch')
+    }
+  }
 
   onRemoveGene (geneName) {
     const { genes } = this.state
@@ -165,8 +165,8 @@ class App extends React.Component {
           onPreviewArtwork={this.onPreviewArtwork}
           onPreviewPrevious={this.onPreviewPrevious}
           onPreviewNext={this.onPreviewNext}
-        */}
           />
+        */}
       </div>
     )
   }
