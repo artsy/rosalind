c36c02f7cd2aad44113c87833a9cf75918f8ebd7
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2017-03-15 17:21:02 -0400

    refactor large render method

--- webpack/apps/original-prototype/components/CurrentCriteria.js
+++ webpack/apps/original-prototype/components/CurrentCriteria.js
@@ -8,17 +8,66 @@ export default function CurrentCriteria (props) {
   const {genes, tags, partner, fair, onRemoveGene, onRemoveTag, onClearPartner, onClearFair} = props
   return (
     <div>
-      { genes.length > 0 && <h2 className='SearchForm-SectionHeader'>Genes</h2> }
-      { genes.map(g => <SelectedGene key={g.id} name={g.name} onRemoveGene={onRemoveGene} />)}
-
-      { tags.length > 0 && <h2 className='SearchForm-SectionHeader'>Tags</h2> }
-      { tags.map(t => <SelectedTag key={t.id} name={t.name} onRemoveTag={onRemoveTag} />)}
-
-      { partner !== null && <h2 className='SearchForm-SectionHeader'>Partner</h2> }
-      { <SelectedPartner partner={partner} onClearPartner={onClearPartner} /> }
-
-      { fair !== null && <h2 className='SearchForm-SectionHeader'>Fair</h2> }
-      { <SelectedFair fair={fair} onClearFair={onClearFair} /> }
+      <CurrentGenes genes={genes} onRemoveGene={onRemoveGene} />
+      <CurrentTags tags={tags} onRemoveTag={onRemoveTag} />
+      <CurrentPartner partner={partner} onClearPartner={onClearPartner} />
+      <CurrentFair fair={fair} onClearFair={onClearFair} />
     </div>
   )
+}
+
+function CurrentGenes (props) {
+  const { genes, onRemoveGene } = props
+  if (genes.length > 0) {
+    return (
+      <div>
+        <h2 className='SearchForm-SectionHeader'>Genes</h2>
+        { genes.map(g => <SelectedGene key={g.id} name={g.name} onRemoveGene={onRemoveGene} />) }
+      </div>
+    )
+  } else {
+    return null
+  }
+}
+
+function CurrentTags (props) {
+  const { tags, onRemoveTag } = props
+  if (tags.length > 0) {
+    return (
+      <div>
+        <h2 className='SearchForm-SectionHeader'>Tags</h2>
+        { tags.map(t => <SelectedTag key={t.id} name={t.name} onRemoveTag={onRemoveTag} />) }
+      </div>
+    )
+  } else {
+    return null
+  }
+}
+
+function CurrentPartner (props) {
+  const { partner, onClearPartner } = props
+  if (partner !== null) {
+    return (
+      <div>
+        <h2 className='SearchForm-SectionHeader'>Partner</h2>
+        <SelectedPartner partner={partner} onClearPartner={onClearPartner} />
+      </div>
+    )
+  } else {
+    return null
+  }
+}
+
+function CurrentFair (props) {
+  const { fair, onClearFair } = props
+  if (fair !== null) {
+    return (
+      <div>
+        <h2 className='SearchForm-SectionHeader'>Fair</h2>
+        <SelectedFair fair={fair} onClearFair={onClearFair} />
+      </div>
+    )
+  } else {
+    return null
+  }
 }
