41d4b6bb8dae4df32a5a01f3a2df9834769ab84a
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2017-03-15 23:51:19 -0400

    add specs for currently selected fair

---
+++ webpack/apps/original-prototype/components/SelectedFair.spec.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { shallow } from 'enzyme'
+import SelectedFair from './SelectedFair'
+
+let props
+
+beforeEach(() => {
+  props = {
+    fair: {
+      id: 'frieze',
+      name: 'Frieze'
+    },
+    onClearFair: jest.fn()
+  }
+})
+
+it('renders correctly', () => {
+  const rendered = renderer.create(<SelectedFair {...props} />)
+  const tree = rendered.toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it('fires the remove handler on click', () => {
+  const wrapper = shallow(<SelectedFair {...props} />)
+  const mockClickEvent = { preventDefault: jest.fn() }
+  wrapper.find('a').simulate('click', mockClickEvent)
+  expect(props.onClearFair.mock.calls.length).toEqual(1)
+})
