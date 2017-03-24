b6cc59cc069fddee0299d99a752ea8dc1ade8207
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2017-03-23 23:54:53 -0400

    Do some minor cleanup and renaming

--- webpack/apps/original-prototype/components/SearchResults.js
+++ webpack/apps/original-prototype/components/SearchResults.js
@@ -2,21 +2,21 @@ import React from 'react'
 import missingImage from 'file-loader!./missing_image.png'
 import ArtworkPreview from './ArtworkPreview'
 
-function SearchResults (props) {
+export default function SearchResults (props) {
   const { artworks, previewedArtwork, onPreviewArtwork, onPreviewPrevious, onPreviewNext } = props
   return (
     <div className='SearchResults'>
       {
-        previewedArtwork ? <ArtworkPreview
+        previewedArtwork && <ArtworkPreview
           artwork={previewedArtwork}
           onPreviewArtwork={onPreviewArtwork}
           onPreviewPrevious={onPreviewPrevious}
           onPreviewNext={onPreviewNext}
-          /> : null
+          />
       }
       {
         artworks.map((artwork) =>
-          <Artwork key={artwork.id}
+          <ArtworkResult key={artwork.id}
             artwork={artwork}
             onPreviewArtwork={onPreviewArtwork}
             />
@@ -26,14 +26,12 @@ function SearchResults (props) {
   )
 }
 
-const Artwork = ({artwork, onPreviewArtwork}) => {
+const ArtworkResult = ({artwork, onPreviewArtwork}) => {
   const { name, image_url: imageUrl } = artwork
   return (
-    <div className='tmp-artwork' onClick={() => { onPreviewArtwork(artwork) }}>
+    <div className='ArtworkResult' onClick={() => { onPreviewArtwork(artwork) }}>
       <img src={imageUrl || missingImage} alt={name} />
       <figcaption>{name}</figcaption>
     </div>
   )
 }
-
-export default SearchResults
