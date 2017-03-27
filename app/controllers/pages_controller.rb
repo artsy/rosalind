dc86198495a24aad83703d389479b2d3b4ded5ae
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2017-03-27 00:39:41 -0400

    Prune early sample code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,3 @@
 class PagesController < ApplicationController
-  def home
-    @artworks = Artwork.list visible: true, sort: '-published_at'
-  end
-
-  def artwork
-    @artwork = Artwork.find params[:id]
-  end
-
   def original_prototype; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#original_prototype'
 
-  # static pages
-  get 'artworks', to: 'pages#home'
-  get 'artwork/:id', to: 'pages#artwork', as: 'artwork'
-
   # searches and autocompletes
   get 'match/artworks'
   get 'match/genes'
