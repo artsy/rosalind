cebf209d6ee318fc286c41c8ae3d49b49c8d4d73
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2017-04-01 00:38:56 -0400

    Rough in a Spinner component when isLoading is true

---
+++ webpack/apps/batch-update/components/Spinner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Spinner () {
+  const overlayStyle = {
+  }
+  const barStyle = {
+  }
+  return (
+    <div style={overlayStyle}>
+      <div style={barStyle}>Spinner!</div>
+    </div>
+  )
+}
