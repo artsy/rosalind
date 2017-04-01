7ead7b32974d5f9275434f83d0500a507360454e
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2017-04-01 00:40:26 -0400

    Style the Spinner element

--- webpack/apps/batch-update/components/Spinner.js
+++ webpack/apps/batch-update/components/Spinner.js
@@ -2,12 +2,27 @@ import React from 'react'
 
 export default function Spinner () {
   const overlayStyle = {
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    right: 0,
+    height: 'calc(100vh - 50px)',
+    background: 'hsla(0, 0%, 100%, 0.90)',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    zIndex: 1
   }
   const barStyle = {
+    width: '12%',
+    height: '1%',
+    background: 'black',
+    alignSelf: 'center',
+    zIndex: 2
   }
   return (
     <div style={overlayStyle}>
-      <div style={barStyle}>Spinner!</div>
+      <div style={barStyle} />
     </div>
   )
 }
