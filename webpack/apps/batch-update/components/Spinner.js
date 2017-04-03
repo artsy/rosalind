0462c4dd4bbaa8653de6f39c9c5c8515c6e622c5
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2017-04-03 12:52:25 -0400

    Extract styles to an external css file

--- webpack/apps/batch-update/components/Spinner.js
+++ webpack/apps/batch-update/components/Spinner.js
@@ -1,29 +1,10 @@
 import React from 'react'
+import './Spinner.css'
 
 export default function Spinner () {
-  const overlayStyle = {
-    position: 'absolute',
-    left: 0,
-    top: 0,
-    right: 0,
-    height: 'calc(100vh - 50px)',
-    background: 'hsla(0, 0%, 100%, 0.90)',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    zIndex: 1
-  }
-  const barStyle = {
-    width: '12%',
-    height: '1%',
-    background: 'black',
-    alignSelf: 'center',
-    animation: 'spin 2s linear infinite',
-    zIndex: 2
-  }
   return (
-    <div style={overlayStyle}>
-      <div style={barStyle} />
+    <div className='Spinner__Overlay'>
+      <div className='Spinner__Line' />
     </div>
   )
 }
