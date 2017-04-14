f8a97a0ce985c94560988673f0e1e6fdbbf1ae83
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2017-04-13 21:14:26 -0400

    Convert FilterOptions to styled components

--- webpack/apps/batch-update/components/FilterOptions.js
+++ webpack/apps/batch-update/components/FilterOptions.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function FilterOptions (props) {
-  const { publishedFilter, deletedFilter, genomedFilter, onSetPublishedFilter, onSetDeletedFilter, onSetGenomedFilter } = props
+function FilterOptions (props) {
+  const { className, publishedFilter, deletedFilter, genomedFilter, onSetPublishedFilter, onSetDeletedFilter, onSetGenomedFilter } = props
   return (
-    <div className='FilterOptions'>
+    <div className={className}>
       <PublishedFilter current={publishedFilter}
         onSetPublishedFilter={onSetPublishedFilter}
         />
@@ -20,7 +20,7 @@ export default function FilterOptions (props) {
 function PublishedFilter (props) {
   const { current, onSetPublishedFilter } = props
   return (
-    <div className='PublishedFilter'>
+    <div className='filter'>
       <div>Published?</div>
       <a href='#' className={current === 'SHOW_ALL' ? 'active' : null} onClick={(e) => { e.preventDefault(); onSetPublishedFilter('SHOW_ALL') }}>All</a>
       <a href='#' className={current === 'SHOW_PUBLISHED' ? 'active' : null} onClick={(e) => { e.preventDefault(); onSetPublishedFilter('SHOW_PUBLISHED') }}>Published</a>
@@ -32,7 +32,7 @@ function PublishedFilter (props) {
 function DeletedFilter (props) {
   const { current, onSetDeletedFilter } = props
   return (
-    <div className='DeletedFilter'>
+    <div className='filter'>
       <div>Deleted?</div>
       <a href='#' className={current === 'SHOW_ALL' ? 'active' : null} onClick={(e) => { e.preventDefault(); onSetDeletedFilter('SHOW_ALL') }}>All</a>
       <a href='#' className={current === 'SHOW_DELETED' ? 'active' : null} onClick={(e) => { e.preventDefault(); onSetDeletedFilter('SHOW_DELETED') }}>Deleted</a>
@@ -44,7 +44,7 @@ function DeletedFilter (props) {
 function GenomedFilter (props) {
   const { current, onSetGenomedFilter } = props
   return (
-    <div className='GenomedFilter'>
+    <div className='filter'>
       <div>Genomed?</div>
       <a href='#' className={current === 'SHOW_ALL' ? 'active' : null} onClick={(e) => { e.preventDefault(); onSetGenomedFilter('SHOW_ALL') }}>All</a>
       <a href='#' className={current === 'SHOW_GENOMED' ? 'active' : null} onClick={(e) => { e.preventDefault(); onSetGenomedFilter('SHOW_GENOMED') }}>Genomed</a>
@@ -52,3 +52,39 @@ function GenomedFilter (props) {
     </div>
   )
 }
+
+/* default styled component */
+
+import styled from 'styled-components'
+
+const StyledFilterOptions = styled(FilterOptions)`
+  font-size: 80%;
+  color: #999;
+  line-height: 140%;
+  margin-top: 2em;
+  opacity: 0.25;
+  transition: opacity 0.75s;
+
+  &:hover {
+    opacity: 1;
+    transition: opacity 0.25s;
+  }
+
+  .filter {
+    margin-top: 1em;
+
+    a {
+      color: #ccc;
+      margin-right: 0.5em;
+
+      &.active {
+        color: #333;
+        font-weight: bold;
+        text-decoration: none;
+        cursor: default;;
+      }
+    }
+  }
+`
+
+export default StyledFilterOptions
