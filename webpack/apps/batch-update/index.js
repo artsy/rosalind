ff8ae233caf690fffb816f708de257d00b0a8b51
Lance W <gnilekaw@protonmail.com>
2017-05-17 10:06:44 -0400

    Add redux to react entrypoint

--- webpack/apps/batch-update/index.js
+++ webpack/apps/batch-update/index.js
@@ -1,8 +1,15 @@
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './containers/App'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './components/App'
+import reducer from './reducers'
+
+const store = createStore(reducer)
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}
+    <App />
+  </Provider>,
   document.getElementById('root')
 )
