346f84420242565374c1d0d42d3ab3713a477f56
Jon Allured <jon.allured@gmail.com>
2017-06-06 10:08:27 -0500

    Remove unnecessary RuboCop exception

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Raven.configure do |config|
   config.dsn = Rails.application.secrets.sentry_dsn
   config.environments = %w[production]
-  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s) # rubocop:disable Metrics/LineLength
+  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
 end
