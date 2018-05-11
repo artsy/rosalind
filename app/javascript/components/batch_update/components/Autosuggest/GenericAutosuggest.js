763a69164738f1dcf10c17ec64bb0cd6bb299b6a
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2018-05-11 17:47:26 -0400

    Move a static css file over to the assets directory
    This is the only static css file in the app, and was inherited
    from ReactAutosuggest.
    
    Webpacker doesn't seem be loading this (even though we appear
    to have CSS loader and this file should be reachable from the
    entry point for this app?)
    
    So I'm just doing this dumb thing right now, and hope to undo it
    one way or another

--- app/javascript/components/batch_update/components/Autosuggest/GenericAutosuggest.js
+++ app/javascript/components/batch_update/components/Autosuggest/GenericAutosuggest.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Autosuggest from 'react-autosuggest'
-import './GenericAutosuggest.css'
 
 export default class GenericAutosuggest extends React.Component {
   constructor (props) {
