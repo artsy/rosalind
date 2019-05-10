c0e6898682ded0ab3172c5b96c92c28180583459
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2019-05-10 17:28:39 -0400

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,15 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true
+    es6: true,
+    jest: true
   },
-  extends: 'eslint:recommended',
+  extends: ['standard', 'standard-react'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly'
   },
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true
@@ -16,5 +18,13 @@ module.exports = {
     sourceType: 'module'
   },
   plugins: ['react'],
-  rules: {}
+  rules: {
+    'react/prop-types': 'off',
+    'space-before-function-paren': ['error', 'never'],
+
+    // next two rules are a workaround, see
+    // https://github.com/babel/babel-eslint/issues/681#issuecomment-420663038
+    indent: 'off',
+    'template-curly-spacing': 'off'
+  }
 }
