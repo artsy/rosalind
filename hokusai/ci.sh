28983cd12946c9d0698b71a1c9c28ae73b78069b
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2019-06-17 17:23:27 -0400

    Get `hokusai test` working

---
+++ hokusai/ci.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ex
+
+# browser testing version info
+google-chrome --version
+
+# set up test database
+bundle exec rails db:drop db:create db:migrate
+
+# run linters
+yarn run lint
+bundle exec rubocop
+
+# run specs
+yarn run test --runInBand
+bundle exec rspec
