2e501fa60f42f634330e216b43ce0639020cf7da
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2022-08-02 17:48:42 -0400

    chore(linting): remove remnants of Standard
    This last bit of config for our pre-Prettier linter probably should have
    been cleaned up in an earlier round of upgrades. Better late than never.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es6: true,
     jest: true
   },
-  extends: ['standard', 'standard-react', 'plugin:prettier/recommended'],
+  extends: ['plugin:prettier/recommended'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly'
