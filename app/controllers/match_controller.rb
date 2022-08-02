23d94afa77a13f25cbc69cb6c59122bcdb9226e5
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2022-08-02 11:52:45 -0400

    feat(filter artworks): update controller to fetch results from Gravity (#671)
    * refactor: extract gravity spec helpers to shared location
    
    * feat: specify and validate allowed params for filter artworks
    
    * feat: add new API endpoint for filter artworks
    
    * refactor: match/filter_artworks --> filter/artworks

--- app/controllers/match_controller.rb
+++ app/controllers/match_controller.rb
@@ -5,6 +5,20 @@ class MatchController < ApplicationController
     render json: response
   end
 
+  def filter_artworks
+    valid_params = params.permit(
+      FilteredArtworkSearchService::ALLOWED_PARAMS,
+      # HACK: Per the Strong Parameters convention, array-valued params
+      # must be mapped to an empty array, so the allowlist above
+      # isn't quite sufficient. We therefore redundantly add…
+      gene_ids: [],
+      artist_ids: []
+    ).to_h
+
+    response = FilteredArtworkSearchService.call(valid_params)
+    render json: response
+  end
+
   def genes
     term = params.require(:term)
     genes = Gene.match term: term, size: 5
--- app/services/filtered_artwork_search_service.rb
+++ app/services/filtered_artwork_search_service.rb
@@ -1,9 +1,27 @@
 module FilteredArtworkSearchService
   class ServiceError < StandardError; end
 
+  ALLOWED_PARAMS = %i[
+    acquireable
+    artist_ids
+    attribution_class
+    fair_id
+    for_sale
+    gene_ids
+    keyword
+    offerable
+    partner_id
+    price_range
+    sort
+    tag_id
+  ].freeze
+
   class << self
     def call(params = {})
+      validate_parameters!(params)
+
       response = Typhoeus.get(api_url, params: params, params_encoding: :rack, headers: headers, accept_encoding: 'gzip')
+
       if response.success?
         JSON.parse(response.body)
       else
@@ -27,6 +45,12 @@ module FilteredArtworkSearchService
         # TODO: replace with user access token, to support fetching unpublished works
         'X-XAPP-TOKEN' => Rails.application.config_for(:gravity)['xapp_token']
       }
+    end
+
+    def validate_parameters!(params)
+      actual_params = params.symbolize_keys.keys
+      unrecognized_params = actual_params - ALLOWED_PARAMS
+      raise ServiceError, "Unrecognized parameters: #{unrecognized_params.inspect}" if unrecognized_params.any?
     end
   end
 end
