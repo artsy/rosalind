5b6c3e3f09e4bf663a9ff72c9326494932cc2c50
Devon Blandin <dblandin@gmail.com>
2024-07-08 17:34:27 +0200

    chore: update deps

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,7 +2,7 @@ Sentry.init do |config|
   config.dsn = Rails.application.secrets.sentry_dsn
   config.enabled_environments = %w[production]
 
-  filter = ActionDispatch::Http::ParameterFilter.new(Rails.application.config.filter_parameters)
+  filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
   config.before_send = lambda do |event, _hint|
     filter.filter(event.to_hash)
   end
